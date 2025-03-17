5f278ea0d494f892d177d020089c6a11d24ef418
amitkundu457 <34626069+amitkundu457@users.noreply.github.com>
2025-03-17 18:55:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-   origin: ["http://localhost:3001"],
+   origin: ["https://radiancehrm.uk"],
    methods: ['GET', 'POST', 'PUT', "DELETE"],
    credentials: true
 }));
